3232f323bf34d08de1752484f940e89bed5bc5a4
genyarko <115112347+genyarko@users.noreply.github.com>
2023-03-10 19:59:12 +0100

    Create 1-calculation.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# Define variables
+a = 10
+b = 5
+
+# Perform calculations
+add_result = add(a, b)
+subtract_result = subtract(a, b)
+multiply_result = multiply(a, b)
+divide_result = divide(a, b)
+
+# Print the results
+print('Add: {}'.format(add_result))
+print('Subtract: {}'.format(subtract_result))
+print('Multiply: {}'.format(multiply_result))
+print('Divide: {}'.format(divide_result))
