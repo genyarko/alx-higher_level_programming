0d740ca3b597709d958f4c427ac8064c9fd06e3c
genyarko <115112347+genyarko@users.noreply.github.com>
2023-03-11 00:58:17 +0100

    Update 102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    result = 0
-    for i in range(1, 3):
-        try:
-            if i > a:
-                raise Exception('Too Far')
-            else:
-                result += (a ** b) / i
-        except:
-            result = b + a
-            break
-    return result
+    from magic_calculation_102 import add, sub
+    c = 0
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        c = sub(a, b)
+        return c
