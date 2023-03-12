4023f2d162f3dbf4dc2c9ee2db2b4b11c21c8fea
genyarko <115112347+genyarko@users.noreply.github.com>
2023-03-12 15:39:39 +0100

    Create 9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+  if len(my_list) == 0:
+    return None
+  else:
+    max_number = my_list[0]
+    for number in my_list:
+      if number > max_number:
+        max_number = number
+    return max_number
