64df90b790dfeb284157035df3af4a05b403bc8f
genyarko <115112347+genyarko@users.noreply.github.com>
2023-03-12 15:32:41 +0100

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
+
 def no_c(my_string):
-    new_string = ''
-    for char in my_string:
-        if char != 'c' and char != 'C':
-            new_string += char
-    return new_string
-
-
+    no_c = my_string.translate({ord('c'): None})
+    no_c = no_c.translate({ord('C'): None})
+    return no_c
