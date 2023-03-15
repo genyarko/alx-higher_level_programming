95444861ac6394c63f867a9dc52ae9cd75638253
genyarko <115112347+genyarko@users.noreply.github.com>
2023-03-15 15:20:51 +0100

    Create 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string): 
+  if not isinstance(roman_string, str) or roman_string == None:
+    return 0 
+  else: 
+    roman_dict = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    total = 0
+    for i in range(len(roman_string)):
+      if i > 0 and roman_dict[roman_string[i]] > roman_dict[roman_string[i-1]]:
+        total += roman_dict[roman_string[i]] - 2 * roman_dict[roman_string[i-1]]
+      else: 
+        total += roman_dict[roman_string[i]]
+    return total
