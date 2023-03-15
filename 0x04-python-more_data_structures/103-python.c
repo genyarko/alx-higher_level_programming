c9df54fcd7394ce09533c2e0cbba49d24aa189e7
genyarko <115112347+genyarko@users.noreply.github.com>
2023-03-15 15:35:05 +0100

    Update 103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,26 +1,6 @@
 #include "/usr/include/python3.4/Python.h"
 #include <stdio.h>
 
-void print_python_list(PyObject *p)
-{
-	if (!p || !PyList_Check(p))
-	{
-		fprintf(stderr, "Error: Invalid List Object\n");
-		return;
-	}
-	
-	int size = PyList_Size(p);
-	fprintf(stdout, "[*] Size of the Python List = %d\n", size);
-	
-	fprintf(stdout, "[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
-	
-	for (int i = 0; i < size; i++)
-	{
-		fprintf(stdout, "[*] Element %d: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
-	}
-}
-
-Python Bytes:
 
 void print_python_bytes(PyObject *p)
 {
@@ -48,5 +28,23 @@ void print_python_bytes(PyObject *p)
 		for (int i = 0; i < size; i++)
 			fprintf(stdout, "%02x ", (unsigned char)PyBytes_AsString(p)[i]);
 		fprintf(stdout, "\n");
+	}
+}
+void print_python_list(PyObject *p)
+{
+	if (!p || !PyList_Check(p))
+	{
+		fprintf(stderr, "Error: Invalid List Object\n");
+		return;
+	}
+	
+	int size = PyList_Size(p);
+	fprintf(stdout, "[*] Size of the Python List = %d\n", size);
+	
+	fprintf(stdout, "[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+	
+	for (int i = 0; i < size; i++)
+	{
+		fprintf(stdout, "[*] Element %d: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
 	}
 }
