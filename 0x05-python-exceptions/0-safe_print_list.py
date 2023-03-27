de0092ea77806ce5aed8dd58ad02d197cf145e72
genyarko <115112347+genyarko@users.noreply.github.com>
2023-03-27 22:29:43 +0200

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,6 +5,7 @@ def safe_print_list(my_list=[], x=0):
     try:
     print(my_list[element], end="")
             elements_printed += 1
+        break
     except:
         print()
     return elements_printed
