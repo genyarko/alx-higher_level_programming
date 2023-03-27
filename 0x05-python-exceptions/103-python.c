21ea1de30e0ef30e136f89e2e1e10eb3ec262d05
genyarko <115112347+genyarko@users.noreply.github.com>
2023-03-28 00:21:01 +0200

    Update 103-python.c

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,7 +1,3 @@
-#include <Python.h>
-void print_python_list(PyObject *p);
-void print_python_bytes(PyObject *p);
-void print_python_float(PyObject *p);
 /**
  * print_python_list - print some basic info about Python lists
  * @p: pointer to a PyObject
