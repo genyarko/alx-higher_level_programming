cf471db6b342f02937e8e3b4dc62f4db5dfd9a78
genyarko <115112347+genyarko@users.noreply.github.com>
2023-03-28 21:24:33 +0200

    Update 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,31 +1,5 @@
 #!/usr/bin/python3
 
-
 class Square:
-    """
-    A class used to represent a Square.
-    
-    Attributes
-    ----------
-    side : int
-        The length of a side of the square.
-    
-    Methods
-    -------
-    area()
-        Returns the area of the square.
-    """
-    def __init__(self, side):
-        """
-        Parameters
-        ----------
-        side : int
-            The length of a side of the square.
-        """
-        self.side = side
-    
-    def area(self):
-        """
-        Returns the area of the square.
-        """
-        return self.side * self.side
+    """ A class used to represent a Square. """
+    pass
