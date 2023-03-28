c819cb5e563d8d39d28bc83bcab20ef08372ff93
genyarko <115112347+genyarko@users.noreply.github.com>
2023-03-28 08:48:19 +0200

    Create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+
+class Square:
+    """
+    A class used to represent a square
+
+    Attributes
+    ----------
+    size : int
+        The side length of the square
+
+    Methods
+    -------
+    area()
+        Returns the area of the square
+    perimeter()
+        Returns the perimeter of the square
+    """
+
+    def __init__(self, size):
+        """
+        Parameters
+        ----------
+        size : int
+            The side length of the square
+        """
+        self.__size = size
+
+    def area(self):
+        """
+        Returns the area of the square
+
+        Returns
+        -------
+        int
+            The area of the square
+        """
+        return self.__size ** 2
+
+    def perimeter(self):
+        """
+        Returns the perimeter of the square
+
+        Returns
+        -------
+        int
+            The perimeter of the square
+        """
+        return self.__size * 4
