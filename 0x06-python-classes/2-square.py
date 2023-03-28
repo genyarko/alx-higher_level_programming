7adf558685d34bea1281a5986c84a1b38e31a982
genyarko <115112347+genyarko@users.noreply.github.com>
2023-03-28 21:27:44 +0200

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,61 +1,20 @@
 #!/usr/bin/python3
+""" defines a square """
 
 
 class Square:
-    """
-    A class used to represent a square
-
-    Attributes
-    ----------
-    size : int
-        The side length of the square
-
-    Methods
-    -------
-    area()
-        Returns the area of the square
-    perimeter()
-        Returns the perimeter of the square
-    """
+    """ square with private instance attribute size """
 
     def __init__(self, size=0):
         """
-        Parameters
-        ----------
-        size : int, optional
-            The side length of the square (default is 0)
-
-        Raises
-        ------
-        TypeError
-            If size is not an integer
-        ValueError
-            If size is less than 0
+        Args:
+            size: size of square
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
 
-    def area(self):
-        """
-        Returns the area of the square
-
-        Returns
-        -------
-        int
-            The area of the square
-        """
-        return self.__size ** 2
-
-    def perimeter(self):
-        """
-        Returns the perimeter of the square
-
-        Returns
-        -------
-        int
-            The perimeter of the square
-        """
-        return self.__size * 4
+        if type(size) is int:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
+        else:
+            raise TypeError('size must be an integer')
