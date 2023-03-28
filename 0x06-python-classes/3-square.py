7b7511de43e51dacf0a46ea75fa9209a36748b87
genyarko <115112347+genyarko@users.noreply.github.com>
2023-03-28 21:31:58 +0200

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 
 
-class Square:
-    """
-    A class used to represent a square
 
+class Square:
+        """
+    A class used to represent a square
     Attributes
     ----------
     size : int
         The side length of the square
-
     Methods
     -------
     area()
@@ -20,42 +19,26 @@ class Square:
 
     def __init__(self, size=0):
         """
-        Parameters
-        ----------
-        size : int, optional
-            The side length of the square (default is 0)
-
-        Raises
-        ------
-        TypeError
-            If size is not an integer
-        ValueError
-            If size is less than 0
+        initializes square
+        Args:
+             The side length of the square (default is 0)
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+
+        if type(size) is int:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
+        else:
+            raise TypeError('size must be an integer')
 
     def area(self):
         """
         Returns the area of the square
-
         Returns
         -------
         int
             The area of the square
         """
+
         return self.__size ** 2
-
-    def perimeter(self):
-        """
-        Returns the perimeter of the square
-
-        Returns
-        -------
-        int
-            The perimeter of the square
-        """
-        return self.__size * 4
