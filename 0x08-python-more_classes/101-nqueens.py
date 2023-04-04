be72ee054e36e79811102b5c2e6dce8638debdb8
genyarko <115112347+genyarko@users.noreply.github.com>
2023-04-04 02:04:22 +0200

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -4,10 +4,31 @@ nqueens backtracking program to print the coordinates of n queens
 on an nxn grid such that they are all in non-attacking positions
 """
 
-from sys import argv
+import sys
+
+def is_safe(board, row, col, N):
+    # Check if no queen can attack the current cell
+    for i in range(row):
+        if board[i] == col or \
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
+            return False
+    return True
+
+def solve(board, row, N):
+    if row == N:
+        # All queens have been placed, print the solution
+        print(board)
+    else:
+        # Try to place a queen in each column of the current row
+        for col in range(N):
+            if is_safe(board, row, col, N):
+                board[row] = col
+                solve(board, row + 1, N)
+                board[row] = -1
 
 def nqueens(N):
-    # Check that N is an integer greater than or equal to 4
+    # Check if N is valid
     if not isinstance(N, int):
         print("N must be a number")
         sys.exit(1)
@@ -15,61 +36,20 @@ def nqueens(N):
         print("N must be at least 4")
         sys.exit(1)
 
-    # Initialize the board
-    board = [[0 for x in range(N)] for y in range(N)]
+    # Initialize the board with -1 in each cell
+    board = [-1] * N
+    solve(board, 0, N)
 
-    # Helper function to check if a move is safe
-    def is_safe(board, row, col):
-        # Check this row on left side
-        for i in range(col):
-            if board[row][i]:
-                return False
-
-        # Check upper diagonal on left side
-        for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-            if board[i][j]:
-                return False
-
-        # Check lower diagonal on left side
-        for i, j in zip(range(row, N), range(col, -1, -1)):
-            if board[i][j]:
-                return False
-
-        # The move is safe
-        return True
-
-    # Recursive function to solve the problem
-    def solve(board, col, solutions):
-        # If all queens are placed
-        if col == N:
-            solutions.append(board)
-            return
-
-        # Try placing this queen in all rows of current column
-        for row in range(N):
-            if is_safe(board, row, col):
-                board[row][col] = 1
-                solve(board, col+1, solutions)
-                board[row][col] = 0
-
-    # Find all solutions
-    solutions = []
-    solve(board, 0, solutions)
-
-    # Print solutions
-    for solution in solutions:
-        print(solution)
-
-if __name__ == "__main__":
-    # Check command line arguments
+if __name__ == '__main__':
+    # Parse command line arguments
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-
     try:
         N = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
 
+    # Solve the problem
     nqueens(N)
