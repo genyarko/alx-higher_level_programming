db8ddb24425cfd7c38086f3c907bb9d2c3885763
genyarko <115112347+genyarko@users.noreply.github.com>
2023-04-12 00:26:40 +0200

    Update 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Contains the lookup function
+a function that returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class ; otherwise False.
 """
 
 def inherits_from(obj, a_class):
