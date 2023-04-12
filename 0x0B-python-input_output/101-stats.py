982bde02178f84274d3b246cf31c422fe6863b3a
genyarko <115112347+genyarko@users.noreply.github.com>
2023-04-13 00:07:40 +0200

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -2,32 +2,27 @@
 
 import sys
 
-total_size = 0
+def print_metrics(total_file_size, status_codes):
+    print("File size: {}".format(total_file_size))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print("{}: {}".format(code, status_codes[code]))
+
 status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+total_file_size = 0
 count = 0
 
 try:
     for line in sys.stdin:
         count += 1
-        split_line = line.split(" ")
-        if len(split_line) > 2:
-            try:
-                status_code = int(split_line[-2])
-                if status_code in status_codes:
-                    status_codes[status_code] += 1
-                total_size += int(split_line[-1])
-            except:
-                pass
-
+        split_line = line.split()
+        status_code = int(split_line[-2])
+        file_size = int(split_line[-1])
+        total_file_size += file_size
+        if status_code in status_codes:
+            status_codes[status_code] += 1
         if count % 10 == 0:
-            print("File size: {}".format(total_size))
-            for code, count in sorted(status_codes.items()):
-                if count > 0:
-                    print("{}: {}".format(code, count))
-
+            print_metrics(total_file_size, status_codes)
 except KeyboardInterrupt:
-    print("File size: {}".format(total_size))
-    for code, count in sorted(status_codes.items()):
-        if count > 0:
-            print("{}: {}".format(code, count))
+    print_metrics(total_file_size, status_codes)
     sys.exit(0)
