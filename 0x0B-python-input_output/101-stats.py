6aee5374b4869f70e4d0ddce0111916be64556d6
genyarko <115112347+genyarko@users.noreply.github.com>
2023-04-13 00:24:29 +0200

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -2,38 +2,30 @@
 """
 reads stdin line by line and computes metrics
 """
+
 import sys
 
-file_size = 0
-status_count = {"200": 0, "301": 0, "400": 0, "401": 0,
-                "403": 0, "404": 0, "405": 0, "500": 0}
-line_count = 0
+def print_metrics(total_file_size, status_codes):
+    print("File size: {}".format(total_file_size))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print("{}: {}".format(code, status_codes[code]))
+
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+total_file_size = 0
+count = 0
 
 try:
     for line in sys.stdin:
-        tokens = line.split()
-
-        if len(tokens) < 5:
-            continue
-
-        status_code = tokens[3]
-        file_size += int(tokens[4])
-        status_count[status_code] += 1
-        line_count += 1
-
-        if line_count % 10 == 0:
-            print("Total file size: File size: {:d}".format(file_size))
-            for code in sorted(status_count.keys()):
-                if status_count[code] > 0:
-                    print("{:s}: {:d}".format(code, status_count[code]))
-
-    print("Total file size: File size: {:d}".format(file_size))
-    for code in sorted(status_count.keys()):
-        if status_count[code] > 0:
-            print("{:s}: {:d}".format(code, status_count[code]))
-
+        count += 1
+        split_line = line.split()
+        status_code = int(split_line[-2])
+        file_size = int(split_line[-1])
+        total_file_size += file_size
+        if status_code in status_codes:
+            status_codes[status_code] += 1
+        if count % 10 == 0:
+            print_metrics(total_file_size, status_codes)
 except KeyboardInterrupt:
-    print("Total file size: File size: {:d}".format(file_size))
-    for code in sorted(status_count.keys()):
-        if status_count[code] > 0:
-            print("{:s}: {:d}".format(code, status_count[code]))
+    print_metrics(total_file_size, status_codes)
+    sys.exit(0)
