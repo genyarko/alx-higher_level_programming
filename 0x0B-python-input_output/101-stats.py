6ee2ea72b98f33c1d158f3b705d42de56cdc455d
genyarko <115112347+genyarko@users.noreply.github.com>
2023-04-13 01:19:35 +0200

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -5,9 +5,9 @@ Reads stdin line by line and computes metrics
 
 import sys
 
-file_size = 0
-status_tally = [0] * 8
-i = 0
+total_file_size = 0
+status_code_counts = [0] * 8
+line_count = 0
 
 try:
     for line in sys.stdin:
@@ -16,21 +16,21 @@ try:
             if tokens[-2].isdigit():
                 status_code = int(tokens[-2])
                 if status_code >= 200 and status_code <= 500:
-                    status_tally[status_code // 100 - 2] += 1
-                    i += 1
+                    status_code_counts[status_code // 100 - 2] += 1
+                    line_count += 1
             if tokens[-1].isdigit():
-                file_size += int(tokens[-1])
-        if i % 10 == 0:
-            print(f"File size: {file_size}")
-            for code, count in enumerate(status_tally):
+                total_file_size += int(tokens[-1])
+        if line_count % 10 == 0:
+            print(f"Total file size: {total_file_size}")
+            for code, count in enumerate(status_code_counts):
                 if count:
                     print(f"{code+200}: {count}")
-    print(f"File size: {file_size}")
-    for code, count in enumerate(status_tally):
+    print(f"Total file size: {total_file_size}")
+    for code, count in enumerate(status_code_counts):
         if count:
             print(f"{code+200}: {count}")
 except KeyboardInterrupt:
-    print(f"File size: {file_size}")
-    for code, count in enumerate(status_tally):
+    print(f"Total file size: {total_file_size}")
+    for code, count in enumerate(status_code_counts):
         if count:
             print(f"{code+200}: {count}")
