02b323f7d278fe98750f93bcc4386c9e5929e0d5
genyarko <115112347+genyarko@users.noreply.github.com>
2023-04-12 22:27:13 +0200

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@ Contains the read_file function
 """
 def read_file(filename=""):
 """""reads a text file(UTF8) and prints it to stdout"""
-    with open(filename, encoding="utf-8") as file:
+    with open(filename,"r", encoding="utf-8") as file:
         for line in file:
             print(line, end="")
