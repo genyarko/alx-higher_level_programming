5856c71cf66002a1e757f6c5a96d7e793c77f5cf
genyarko <115112347+genyarko@users.noreply.github.com>
2023-04-13 00:55:40 +0200

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -5,43 +5,29 @@ reads stdin line by line and computes metrics
 
 
 import sys
+from collections import defaultdict
 
-status_tally = [0] * 8
 file_size = 0
-i = 0
+status_tally = defaultdict(int)
 
 try:
-    for line in sys.stdin:
-        try:
-            _, _, _, status, size = line.rsplit(maxsplit=4)
-        except ValueError:
-            continue
-
-        try:
-            size = int(size)
-        except ValueError:
-            continue
-
-        file_size += size
-
-        status_code = int(status[0] + '00')
-        if status_code in [200, 300, 400, 500]:
-            status_tally[(status_code // 100) - 2] += 1
-
-        i += 1
+    for i, line in enumerate(sys.stdin, 1):
+        tokens = line.split()
+        if len(tokens) >= 2 and tokens[-2] in status_tally:
+            status_tally[tokens[-2]] += 1
+            file_size += int(tokens[-1])
         if i % 10 == 0:
             print(f"File size: {file_size}")
-            for j in range(len(status_tally)):
-                if status_tally[j] > 0:
-                    print(f"{(j+2)*100}: {status_tally[j]}")
-
+            for key in sorted(status_tally.keys()):
+                if status_tally[key]:
+                    print(f"{key}: {status_tally[key]}")
     print(f"File size: {file_size}")
-    for j in range(len(status_tally)):
-        if status_tally[j] > 0:
-            print(f"{(j+2)*100}: {status_tally[j]}")
+    for key in sorted(status_tally.keys()):
+        if status_tally[key]:
+            print(f"{key}: {status_tally[key]}")
 
 except KeyboardInterrupt:
-    print(f"\nFile size: {file_size}")
-    for j in range(len(status_tally)):
-        if status_tally[j] > 0:
-            print(f"{(j+2)*100}: {status_tally[j]}")
+    print(f"File size: {file_size}")
+    for key in sorted(status_tally.keys()):
+        if status_tally[key]:
+            print(f"{key}: {status_tally[key]}")
