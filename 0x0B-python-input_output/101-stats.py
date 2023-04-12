ae54faf7b12ffffa20d9406c86c940e1e92f6a49
genyarko <115112347+genyarko@users.noreply.github.com>
2023-04-13 01:27:32 +0200

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-Reads stdin line by line and computes metrics
+Reads stdin line by line and computes metrics.
 """
 
 import sys
 
 total_file_size = 0
-status_code_counts = [0] * 8
+status_counts = [0] * 8
 line_count = 0
 
 try:
@@ -16,22 +16,22 @@ try:
             if tokens[-2].isdigit():
                 status_code = int(tokens[-2])
                 if status_code >= 200 and status_code <= 500:
-                    status_code_counts[status_code // 100 - 2] += 1
+                    status_counts[status_code // 100 - 2] += 1
                     line_count += 1
             if tokens[-1].isdigit():
-                total_file_size += int(tokens[-1])
+                file_size = int(tokens[-1])
+                total_file_size += file_size
         if line_count % 10 == 0:
             print(f"Total file size: {total_file_size}")
-            for code, count in enumerate(status_code_counts):
+            for code, count in enumerate(status_counts):
                 if count:
                     print(f"{code+200}: {count}")
     print(f"Total file size: {total_file_size}")
-    for code, count in enumerate(status_code_counts):
+    for code, count in enumerate(status_counts):
         if count:
             print(f"{code+200}: {count}")
 except KeyboardInterrupt:
     print(f"Total file size: {total_file_size}")
-    for code, count in enumerate(status_code_counts):
+    for code, count in enumerate(status_counts):
         if count:
             print(f"{code+200}: {count}")
-
