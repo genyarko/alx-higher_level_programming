25b0f184ae267de3d7f06c994592386b39171702
genyarko <115112347+genyarko@users.noreply.github.com>
2023-04-13 01:15:16 +0200

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,6 +4,11 @@ Reads stdin line by line and computes metrics
 """
 
 
+#!/usr/bin/python3
+"""
+reads stdin line by line and computes metrics
+"""
+
 import sys
 
 file_size = 0
@@ -14,13 +19,13 @@ try:
     for line in sys.stdin:
         tokens = line.split()
         if len(tokens) >= 2:
-            status_code = tokens[-2]
-            if status_code.isdigit():
-                status_code = int(status_code)
-                if 200 <= status_code <= 500:
+            if tokens[-2].isdigit():
+                status_code = int(tokens[-2])
+                if status_code >= 200 and status_code <= 500:
                     status_tally[status_code // 100 - 2] += 1
                     i += 1
-            file_size += int(tokens[-1]) if tokens[-1].isdigit() else 0
+            if tokens[-1].isdigit():
+                file_size += int(tokens[-1])
         if i % 10 == 0:
             print(f"File size: {file_size}")
             for code, count in enumerate(status_tally):
