e39f363867bfe1c329d7f870cc6879de62c18ec2
genyarko <115112347+genyarko@users.noreply.github.com>
2023-04-12 23:03:37 +0200

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,20 +3,8 @@
 a script
 """
 
-import sys
 import json
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
 
-# Try to load existing data from file
-try:
-    data = load_from_json_file("add_item.json")
-except:
-    data = []
-
-# Add new arguments to the list
-for arg in sys.argv[1:]:
-    data.append(arg)
-
-# Save the updated data to file
-save_to_json_file(data, "add_item.json")
+def load_from_json_file(filename):
+    with open(filename, mode='r', encoding='utf-8') as f:
+        return json.load(f)
