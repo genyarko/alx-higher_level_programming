45c42a28c55d27817464eb25c358eee215889b23
genyarko <115112347+genyarko@users.noreply.github.com>
2023-04-13 00:50:18 +0200

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -6,25 +6,34 @@ reads stdin line by line and computes metrics
 import sys
 from collections import defaultdict
 
-def print_stats(file_size, status_tally):
-    print("File size: {:d}".format(file_size))
-    for key, value in sorted(status_tally.items()):
-        if value:
-            print("{:s}: {:d}".format(key, value))
-
 file_size = 0
 status_tally = defaultdict(int)
+i = 0
+
 try:
-    for i, line in enumerate(sys.stdin, start=1):
+    for line in sys.stdin:
         tokens = line.split()
-        if len(tokens) >= 2 and tokens[-2] in status_tally:
-            status_tally[tokens[-2]] += 1
-        try:
-            file_size += int(tokens[-1])
-        except (IndexError, ValueError):
-            pass
+        if len(tokens) >= 2:
+            if tokens[-2] in status_tally:
+                status_tally[tokens[-2]] += 1
+                i += 1
+            try:
+                file_size += int(tokens[-1])
+                i += 1
+            except (ValueError, FileNotFoundError):
+                pass
         if i % 10 == 0:
-            print_stats(file_size, status_tally)
-    print_stats(file_size, status_tally)
+            print(f"File size: {file_size}")
+            for key, value in sorted(status_tally.items()):
+                if value:
+                    print(f"{key}: {value}")
+    print(f"File size: {file_size}")
+    for key, value in sorted(status_tally.items()):
+        if value:
+            print(f"{key}: {value}")
+
 except KeyboardInterrupt:
-    print_stats(file_size, status_tally)
+    print(f"File size: {file_size}")
+    for key, value in sorted(status_tally.items()):
+        if value:
+            print(f"{key}: {value}")
