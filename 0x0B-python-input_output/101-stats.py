5553d1a5a1510fe87a5c6fb42459e12fa206b102
genyarko <115112347+genyarko@users.noreply.github.com>
2023-04-13 00:51:44 +0200

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,36 +4,36 @@ reads stdin line by line and computes metrics
 """
 
 import sys
-from collections import defaultdict
+
+STATUS_CODES = ("200", "301", "400", "401", "403", "404", "405", "500")
+
+def print_stats(file_size, status_tally):
+    print("File size: {:d}".format(file_size))
+    for key, value in sorted(status_tally.items()):
+        if value:
+            print("{:s}: {:d}".format(key, value))
 
 file_size = 0
-status_tally = defaultdict(int)
-i = 0
+status_tally = {code: 0 for code in STATUS_CODES}
+line_count = 0
 
 try:
     for line in sys.stdin:
+        line_count += 1
         tokens = line.split()
-        if len(tokens) >= 2:
-            if tokens[-2] in status_tally:
-                status_tally[tokens[-2]] += 1
-                i += 1
+        if len(tokens) >= 7:
+            status_code = tokens[-2]
+            if status_code in status_tally:
+                status_tally[status_code] += 1
             try:
                 file_size += int(tokens[-1])
-                i += 1
-            except (ValueError, FileNotFoundError):
+            except ValueError:
                 pass
-        if i % 10 == 0:
-            print(f"File size: {file_size}")
-            for key, value in sorted(status_tally.items()):
-                if value:
-                    print(f"{key}: {value}")
-    print(f"File size: {file_size}")
-    for key, value in sorted(status_tally.items()):
-        if value:
-            print(f"{key}: {value}")
+
+        if line_count % 10 == 0:
+            print_stats(file_size, status_tally)
+
+    print_stats(file_size, status_tally)
 
 except KeyboardInterrupt:
-    print(f"File size: {file_size}")
-    for key, value in sorted(status_tally.items()):
-        if value:
-            print(f"{key}: {value}")
+    print_stats(file_size, status_tally)
