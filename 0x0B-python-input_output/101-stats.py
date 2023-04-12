a0cf3d1e7a4c502b87a18aeefa219c74fbbfa2db
genyarko <115112347+genyarko@users.noreply.github.com>
2023-04-13 01:08:18 +0200

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -5,38 +5,37 @@ reads stdin line by line and computes metrics
 
 import sys
 
-# use a list with fixed size instead of a dictionary
-status_tally = [0] * 8
-# index in list corresponds to status code, so we can increment directly
-
+# Initialize variables
 file_size = 0
+status_tally = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+count = 0
 
+# Process input line by line
 try:
-    for i, line in enumerate(sys.stdin, start=1):
+    for line in sys.stdin:
         tokens = line.split()
-        if len(tokens) >= 2:
-            if tokens[-2].isdigit():
-                # use index in list to increment status code tally
-                status_tally[int(tokens[-2]) // 100 - 2] += 1
-            try:
-                file_size += int(tokens[-1])
-            except ValueError:
-                pass
-        if i % 10 == 0:
-            # move print statements outside of the loop
-            print("File size: {:d}".format(file_size))
-            for code, count in enumerate(status_tally, start=2):
-                if count:
-                    print(f"{code}00: {count}")
-    # move print statements outside of the loop
-    print("File size: {:d}".format(file_size))
-    for code, count in enumerate(status_tally, start=2):
-        if count:
-            print(f"{code}00: {count}")
 
+        # Only process valid input lines
+        if len(tokens) >= 7:
+            # Update file size
+            file_size += int(tokens[-1])
+
+            # Update status code count
+            status_code = tokens[-2]
+            if status_code in status_tally:
+                status_tally[status_code] += 1
+
+        # Print metrics every 10 lines
+        count += 1
+        if count % 10 == 0:
+            print("File size: {:d}".format(file_size))
+            for code in sorted(status_tally.keys()):
+                if status_tally[code] > 0:
+                    print("{}: {}".format(code, status_tally[code]))
+
+# Handle keyboard interrupt gracefully
 except KeyboardInterrupt:
-    # move print statements outside of the loop
     print("File size: {:d}".format(file_size))
-    for code, count in enumerate(status_tally, start=2):
-        if count:
-            print(f"{code}00: {count}")
+    for code in sorted(status_tally.keys()):
+        if status_tally[code] > 0:
+            print("{}: {}".format(code, status_tally[code]))
