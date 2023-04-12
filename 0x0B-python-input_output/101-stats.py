a920f06f2551b4ca6b70ecaaf99e52e9ac971af1
genyarko <115112347+genyarko@users.noreply.github.com>
2023-04-13 00:46:55 +0200

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,38 +3,31 @@
 reads stdin line by line and computes metrics
 """
 import sys
+from collections import defaultdict
 
-# Initialize dictionary and variables
-status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
-total_size = 0
-line_count = 0
-
-try:
-    while True:
-        # Read input from stdin
-        line = sys.stdin.readline().strip()
-
-        # Extract status code and file size
-        parts = line.split()
-        status_code = parts[3]
-        file_size = int(parts[4])
-
-        # Update dictionary and variables
-        status_codes[status_code] += 1
-        total_size += file_size
-        line_count += 1
-
-        # Print statistics every 10 lines
-        if line_count % 10 == 0:
-            print(f"Total file size: {total_size}")
-            for code in sorted(status_codes.keys()):
-                if status_codes[code] > 0:
-                    print(f"{code}: {status_codes[code]}")
-
-except KeyboardInterrupt:
-    # Terminate gracefully on keyboard interrupt
-    print("\nFinal statistics:")
+def print_stats(total_size, status_codes):
     print(f"Total file size: {total_size}")
     for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print(f"{code}: {status_codes[code]}")
+        print(f"{code}: {status_codes[code]}")
+
+def parse_line(line):
+    parts = line.split()
+    ip_address = parts[0]
+    status_code = int(parts[3])
+    file_size = int(parts[4])
+    return ip_address, status_code, file_size
+
+total_size = 0
+status_codes = defaultdict(int)
+count = 0
+
+try:
+    for line in sys.stdin:
+        ip_address, status_code, file_size = parse_line(line)
+        total_size += file_size
+        status_codes[status_code] += 1
+        count += 1
+        if count % 10 == 0:
+            print_stats(total_size, status_codes)
+except KeyboardInterrupt:
+    print_stats(total_size, status_codes)
