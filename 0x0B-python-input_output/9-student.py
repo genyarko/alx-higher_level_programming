95fff92a51e1d0d9fcddabcc76918825ab8f58dd
genyarko <115112347+genyarko@users.noreply.github.com>
2023-04-12 23:09:24 +0200

    Create 9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Contains the clas "Student"
+"""
+class Student:
+    """
+    Student class definition
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new instance of the Student class
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance
+        """
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
