36495c526047cbca4a5ff04a9da55deaac6c81d8
genyarko <115112347+genyarko@users.noreply.github.com>
2023-04-13 00:32:54 +0200

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -5,54 +5,37 @@ reads stdin line by line and computes metrics
 
 import sys
 
-# Initialize variables for metrics
-total_file_size = 0
-status_codes = {
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0,
-}
+file_size = 0
+status_count = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
 line_count = 0
 
 try:
-    # Read from standard input line by line
     for line in sys.stdin:
-        # Split the line into its components
-        ip_address, _, date, request, status_code_str, file_size_str = line.split()
+        tokens = line.split()
 
-        # Convert the status code and file size to integers
-        status_code = int(status_code_str)
-        file_size = int(file_size_str)
+        if len(tokens) < 5:
+            continue
 
-        # Add the file size to the total
-        total_file_size += file_size
-
-        # Increment the count for the status code
-        if status_code in status_codes:
-            status_codes[status_code] += 1
-
-        # Increment the line count
+        status_code = tokens[3]
+        file_size += int(tokens[4])
+        status_count[status_code] += 1
         line_count += 1
 
-        # Every 10 lines, print the metrics so far
         if line_count % 10 == 0:
-            print(f"Total file size: {total_file_size}")
-            for code in sorted(status_codes.keys()):
-                count = status_codes[code]
-                if count > 0:
-                    print(f"{code}: {count}")
+            print("Total file size: File size: {:d}".format(file_size))
+            for code in sorted(status_count.keys()):
+                if status_count[code] > 0:
+                    print("{:s}: {:d}".format(code, status_count[code]))
+
+    print("Total file size: File size: {:d}".format(file_size))
+    for code in sorted(status_count.keys()):
+        if status_count[code] > 0:
+            print("{:s}: {:d}".format(code, status_count[code]))
 
 except KeyboardInterrupt:
-    # If there's a keyboard interruption, print the metrics and exit
-    print(f"Total file size: {total_file_size}")
-    for code in sorted(status_codes.keys()):
-        count = status_codes[code]
-        if count > 0:
-            print(f"{code}: {count}")
-    sys.exit(0)
+    print("Total file size: File size: {:d}".format(file_size))
+    for code in sorted(status_count.keys()):
+        if status_count[code] > 0:
+            print("{:s}: {:d}".format(code, status_count[code]))
 
