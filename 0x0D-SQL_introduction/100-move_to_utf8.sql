d30d3eed07837ec039a7748ce390718e18e7ec01
genyarko <115112347+genyarko@users.noreply.github.com>
2023-05-16 14:43:52 +0200

    Update 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,11 +2,6 @@
 
 -- Description: This script converts the hbtn_0c_0 database, first_table table, and name field to UTF8 (utf8mb4, collate utf8mb4_unicode_ci).
 
--- Step 1: Convert the database to UTF8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Step 2: Convert the table to UTF8
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Step 3: Convert the name field to UTF8
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `hbtn_0c_0`
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
