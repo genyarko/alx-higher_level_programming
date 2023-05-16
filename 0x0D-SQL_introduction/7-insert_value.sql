166305d9aa4c29ede9aa363fa5c5acabe7356931
genyarko <115112347+genyarko@users.noreply.github.com>
2023-05-16 14:25:41 +0200

    Create 7-insert_value.sql

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,7 @@
+-- Script: Insert New Row into first_table
+
+-- Description: This script inserts a new row into the table first_table in the hbtn_0c_0 database in the MySQL server.
+
+-- SQL query to insert a new row into the first_table
+INSERT INTO hbtn_0c_0.first_table (id, name)
+VALUES (89, 'Best School');
