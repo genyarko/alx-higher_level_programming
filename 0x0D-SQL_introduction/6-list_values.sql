f6a97a7c2a864e8d28d7ae006e6356a019c2cbe7
genyarko <115112347+genyarko@users.noreply.github.com>
2023-05-16 14:21:50 +0200

    Create 6-list_values.sql

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,7 @@
+-- Script: List Rows of first_table
+
+-- Description: This script lists all rows of the table first_table from the hbtn_0c_0 database in the MySQL server.
+
+-- SQL query to retrieve all rows from the first_table
+SELECT *
+FROM hbtn_0c_0.first_table;
