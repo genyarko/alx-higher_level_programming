176018ff5176c38686c1c7cc9353ce44c1e20c01
genyarko <115112347+genyarko@users.noreply.github.com>
2023-05-16 14:22:44 +0200

    Update 5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -3,6 +3,4 @@
 -- Description: This script prints the full description of the table first_table from the hbtn_0c_0 database in the MySQL server.
 
 -- SQL query to retrieve the table description
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, COLUMN_KEY, IS_NULLABLE
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE `first_table`;
