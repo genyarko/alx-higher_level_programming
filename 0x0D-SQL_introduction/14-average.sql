574be8a242009383d58b9e187ef1003f1d6dd2e1
genyarko <115112347+genyarko@users.noreply.github.com>
2023-05-16 14:37:39 +0200

    Create 14-average.sql

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,7 @@
+-- Script: Compute Average Score
+
+-- Description: This script computes the average score of all records in the table second_table of the hbtn_0c_0 database.
+
+-- SQL query to compute the average score of all records
+SELECT AVG(score) AS average
+FROM second_table;
