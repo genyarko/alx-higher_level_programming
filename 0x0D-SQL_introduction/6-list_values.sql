e91f08b78730852052188fb8ca6a91a90a9c3f04
genyarko <115112347+genyarko@users.noreply.github.com>
2023-05-16 14:23:29 +0200

    Update 6-list_values.sql

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -3,5 +3,4 @@
 -- Description: This script lists all rows of the table first_table from the hbtn_0c_0 database in the MySQL server.
 
 -- SQL query to retrieve all rows from the first_table
-SELECT *
-FROM hbtn_0c_0.first_table;
+SELECT * FROM `first_table`;
