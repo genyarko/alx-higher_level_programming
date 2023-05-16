ccce0e653b87c8cd4406cc1e65d140cb732f0ce4
genyarko <115112347+genyarko@users.noreply.github.com>
2023-05-16 14:30:30 +0200

    Update 9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,19 @@
--- Script: Count Records with id = 89
+-- Script: Create second_table and Add Multiple Rows
 
--- Description: This script displays the number of records with id = 89 in the table first_table of the hbtn_0c_0 database.
+-- Description: This script creates the table second_table in the hbtn_0c_0 database and adds multiple rows to it.
 
--- SQL query to count the number of records with id = 89
-SELECT COUNT(*) AS record_count
-FROM first_table
-WHERE id = 89;
+-- SQL query to create the second_table if it doesn't exist
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+-- SQL query to add multiple rows to the second_table
+INSERT INTO second_table (id, name, score)
+VALUES
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);
+
