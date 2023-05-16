2c49b2f232a3523004700a8f7388e4b2bd5077c0
genyarko <115112347+genyarko@users.noreply.github.com>
2023-05-16 14:08:43 +0200

    Update 0-list_databases.sql

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,12 +1,5 @@
 -- Script: List Databases
 -- Description: This script lists all databases on the MySQL server.
 
--- Connect to the MySQL server
--- Replace 'username' and 'password' with your MySQL credentials
--- Replace 'hostname' with the actual hostname or IP address of your MySQL server
--- Replace 'port' with the port number used by your MySQL server (default is 3306)
-mysql -u username -p'password' -h hostname -P port -e "
--- SQL query to retrieve all databases
-SELECT DISTINCT TABLE_SCHEMA AS DatabaseName
-FROM INFORMATION_SCHEMA.TABLES;
-"
+-- lists all databases of your MySQL server.
+SHOW DATABASES;
