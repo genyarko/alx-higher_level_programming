0c2109f526b191367b55e068bb0f4eebf7ba227a
genyarko <115112347+genyarko@users.noreply.github.com>
2023-05-16 14:27:40 +0200

    Create 8-count_89.sql

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,8 @@
+-- Script: Count Records with id = 89
+
+-- Description: This script displays the number of records with id = 89 in the table first_table of the hbtn_0c_0 database.
+
+-- SQL query to count the number of records with id = 89
+SELECT COUNT(*) AS record_count
+FROM first_table
+WHERE id = 89;
