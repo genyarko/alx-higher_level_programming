fd0e1d345f664364b821c30b5f89c3e77e74eade
genyarko <115112347+genyarko@users.noreply.github.com>
2023-05-17 21:12:09 +0200

    Update 8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,10 @@
 -- Use the hbtn_0d_usa database
-USE hbtn_0d_usa;
 
--- Get the state_id for California from the states table
-SET @state_id := (SELECT id FROM states WHERE name = 'California');
 
--- List all cities of California from the cities table
-SELECT * FROM cities WHERE state_id = @state_id ORDER BY id ASC;
+SELECT `id`, `name`
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	  FROM `states`
+	 WHERE `name` = "California")
+ ORDER BY `id`;
