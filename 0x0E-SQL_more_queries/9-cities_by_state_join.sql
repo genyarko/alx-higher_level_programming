b2d9dbcb627234e291df306fb9f1d80b0dcd5efb
genyarko <115112347+genyarko@users.noreply.github.com>
2023-05-17 21:14:37 +0200

    Update 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,8 +1,7 @@
--- Use the hbtn_0d_usa database
-USE hbtn_0d_usa;
-
 -- List all cities with the required format
-SELECT CONCAT(cities.id, ' - ', cities.name, ' - ', states.name) AS city_info
-FROM cities
-JOIN states ON cities.state_id = states.id
-ORDER BY cities.id ASC;
+SELECT c.`id`, c.`name`, s.`name`
+  FROM `cities` AS c
+       INNER JOIN `states` AS s
+       ON c.`state_id` = s.`id`
+ ORDER BY c.`id`;
+
