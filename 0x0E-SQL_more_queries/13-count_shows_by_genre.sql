0b03862c55371171916a0b09a93e885c218c9e7b
genyarko <115112347+genyarko@users.noreply.github.com>
2023-05-17 21:27:47 +0200

    Update 13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,8 @@
 -- List shows without a genre linked
-SELECT g.genre AS genre, COUNT(*) AS number_of_shows
-FROM tv_genres AS g
-INNER JOIN tv_show_genres AS sg ON g.id = sg.genre_id
-GROUP BY g.genre
-ORDER BY number_of_shows DESC;
+SELECT g.`name` AS `genre`,
+       COUNT(*) AS `number_of_shows`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS t
+       ON g.`id` = t.`genre_id`
+ GROUP BY g.`name`
+ ORDER BY `number_of_shows` DESC;
