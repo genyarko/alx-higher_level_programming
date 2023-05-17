8d2b779e37c234bdb1257a443dc91a437aac4832
genyarko <115112347+genyarko@users.noreply.github.com>
2023-05-17 21:17:30 +0200

    Update 10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,8 +1,6 @@
--- Use the hbtn_0d_tvshows database
-USE hbtn_0d_tvshows;
-
 -- List shows with linked genres
-SELECT CONCAT(tv_shows.title, ' - ', tv_show_genres.genre_id) AS show_info
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+        INNER JOIN `tv_show_genres` AS g
+	ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
