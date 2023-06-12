b22ab1d51c8ac1e9fa46904daa70aadb02c45159
genyarko <115112347+genyarko@users.noreply.github.com>
2023-06-12 11:23:57 +0200

    Create 5-to_integer.js

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const arg = process.argv[2];
+const converted = parseInt(arg);
+
+if (!isNaN(converted)) {
+  console.log("My number: " + converted);
+} else {
+  console.log("Not a number");
+}
