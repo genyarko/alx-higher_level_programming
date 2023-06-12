1f08c59b0d7d422caac036021f913947f1bf161a
genyarko <115112347+genyarko@users.noreply.github.com>
2023-06-12 15:57:22 +0200

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,13 @@
 #!/usr/bin/node
-const count = process.argv.length;
-const message =
-  count === 2 ? 'No argument' :
-  count === 3 ? 'Argument found' :
-  'Arguments found';
-console.log(message);
+
+const args = process.argv.slice(2);
+const numArgs = args.length;
+
+if (numArgs === 0) {
+  console.log('No argument');
+} else if (numArgs === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
+
