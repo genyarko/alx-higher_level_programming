c8715490791db177c7a5bd0047ed3f63c4fb7669
genyarko <115112347+genyarko@users.noreply.github.com>
2023-06-12 13:12:17 +0200

    Create 7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const arg = process.argv[2];
+const numOccurrences = parseInt(arg);
+
+if (!isNaN(numOccurrences)) {
+  for (let i = 0; i < numOccurrences; i++) {
+    console.log("C is fun");
+  }
+} else {
+  console.log("Missing number of occurrences");
+}
