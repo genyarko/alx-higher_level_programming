5dc3dedb59d75add0d97d73f1cb72d966dad2409
genyarko <115112347+genyarko@users.noreply.github.com>
2023-06-13 11:51:08 +0200

    Create 4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,42 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return {};
+    }
+
+    this.width = w;
+    this.height = h;
+  }
+
+  print() {
+    if (Object.keys(this).length === 0) {
+      return;
+    }
+
+    const row = 'X'.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
+
+  rotate() {
+    if (Object.keys(this).length === 0) {
+      return;
+    }
+
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double() {
+    if (Object.keys(this).length === 0) {
+      return;
+    }
+
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
