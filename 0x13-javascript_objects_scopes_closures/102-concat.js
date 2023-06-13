df65482688f5337ba769a9e64b77b6e3e962cda1
genyarko <115112347+genyarko@users.noreply.github.com>
2023-06-13 17:03:09 +0200

    Update 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,32 @@
 #!/usr/bin/node
+
 const fs = require('fs');
 
-const fArg = fs.readFileSync(process.argv[2]).toString();
-const sArg = fs.readFileSync(process.argv[3]).toString();
-fs.writeFileSync(process.argv[4], fArg + sArg);
+const sourceFile1 = process.argv[2];
+const sourceFile2 = process.argv[3];
+const destinationFile = process.argv[4];
+
+fs.readFile(sourceFile1, 'utf8', (err, data1) => {
+  if (err) {
+    console.error(`Error reading source file 1: ${err}`);
+    return;
+  }
+
+  fs.readFile(sourceFile2, 'utf8', (err, data2) => {
+    if (err) {
+      console.error(`Error reading source file 2: ${err}`);
+      return;
+    }
+
+    const concatenatedContent = data1 + data2;
+
+    fs.writeFile(destinationFile, concatenatedContent, 'utf8', (err) => {
+      if (err) {
+        console.error(`Error writing to destination file: ${err}`);
+        return;
+      }
+
+      console.log('Concatenation successful. Destination file created.');
+    });
+  });
+});
