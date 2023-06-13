a74167e411870d039825d7c7dbc5d39a8f158dd7
genyarko <115112347+genyarko@users.noreply.github.com>
2023-06-13 11:37:11 +0200

    Create 3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return {};
+    }
+
+    this.width = w;
+    this.height = h;
+  }
+
+  print() {
+    if (Object.keys(this).length === 0) {
+      return;
+    }
+
+    const row = 'X'.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
+}
+
+module.exports = Rectangle;
