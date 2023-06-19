26203f5a62aa20dfc3fcd949ce5ff5fe20cc8d2d
genyarko <115112347+genyarko@users.noreply.github.com>
2023-06-19 02:17:34 +0200

    Add files via upload

--- 0x0F-python-object_relational_mapping/6-model_state.sql
+++ 0x0F-python-object_relational_mapping/6-model_state.sql
@@ -1,10 +1,4 @@
-from sqlalchemy import create_engine
-from models import State
-
-# Create the engine to connect to your MySQL server
-engine = create_engine('mysql://username:password@localhost:3306/database')
-
-# Import all classes that inherit from Base
-
-# Call create_all() to create the tables
-Base.metadata.create_all(engine)
+-- Create database hbtn_0e_6_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0e_6_usa;
+USE hbtn_0e_6_usa;
+SHOW CREATE TABLE states;
