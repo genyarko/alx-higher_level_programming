7ec0616a439122f91481a72cb721a766751c1c15
genyarko <115112347+genyarko@users.noreply.github.com>
2023-06-27 18:40:52 +0200

    Add files via upload

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# This script takes a URL as the first argument, sends a DELETE request to that URL using curl,
-# and displays the body of the response.
-
-URL="$1"
-
-# Send the DELETE request using curl with the -s (silent mode) and -X DELETE options
-response=$(curl -s -X DELETE "$URL")
-
-# Display the body of the response
-echo "$response"
+# Send a DELETE request to a given URL and display the response body.
+curl -sX DELETE "$1"
