872407a7dfb2cb5856c67299d44500ddae89db00
genyarko <115112347+genyarko@users.noreply.github.com>
2023-06-30 21:44:34 +0200

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
+"""Sends a request to a given URL and displays the response body.
 
-import urllib.request
-import urllib.error
+Usage: ./4-hbtn_status.py <URL>
+    - Displays the body of the response (decoded in utf-8).
+    - Handles HTTP errors and prints the error code.
+"""
+
 import sys
+import urllib.error
+import urllib.request
 
-url = sys.argv[1]
 
-try:
-    with urllib.request.urlopen(url) as response:
-        body = response.read().decode('utf-8')
-    print(body)
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-except urllib.error.HTTPError as e:
-    print("Error code:", e.code)
+    try:
+        with urllib.request.urlopen(url) as response:
+            print(response.read().decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
+
