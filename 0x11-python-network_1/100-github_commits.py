83c2acbf665e7689c64b8547b1672c81941f2c86
genyarko <115112347+genyarko@users.noreply.github.com>
2023-06-30 22:09:22 +0200

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-
-import requests
+"""Lists the 10 most recent commits on a given GitHub repository.
+Usage: ./100-github_commits.py <repository name> <repository owner>
+"""
 import sys
+import requests
 
-repository = sys.argv[1]
-owner = sys.argv[2]
 
-url = f'https://api.github.com/repos/{owner}/{repository}/issues'
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-response = requests.get(url)
-
-if response.status_code == 200:
-    issues = response.json()
-    for issue in issues:
-        print("Issue:", issue['title'])
-else:
-    print("Failed to retrieve issues. Status code:", response.status_code)
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
