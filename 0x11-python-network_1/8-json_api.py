dfbeb885c45659ad1db641ea881572359b996165
genyarko <115112347+genyarko@users.noreply.github.com>
2023-06-30 22:04:30 +0200

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
-
-import requests
+"""
+Sends a POST request to http://0.0.0.0:5000/search_user with a given letter as a parameter.
+Usage: ./8-json_api.py <letter>
+"""
 import sys
+import requests
 
-url = "http://0.0.0.0:5000/search_user"
-letter = sys.argv[1] if len(sys.argv) > 1 else ""
 
-data = {'q': letter}
-response = requests.post(url, data=data)
-
-try:
-    json_data = response.json()
-    if json_data:
-        print("[{}] {}".format(json_data['id'], json_data['name']))
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        q = ""
     else:
-        print("No result")
-except ValueError:
-    print("Not a valid JSON")
+        q = sys.argv[1]
+
+    data = {"q": q}
+    response = requests.post("http://0.0.0.0:5000/search_user", data=data)
+
+    try:
+        json_data = response.json()
+        if json_data:
+            print("[{}] {}".format(json_data.get("id"), json_data.get("name")))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
+
