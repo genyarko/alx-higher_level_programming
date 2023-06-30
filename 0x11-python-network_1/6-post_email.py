ae40e67ed4238819de28d36fa58ea38a54855978
genyarko <115112347+genyarko@users.noreply.github.com>
2023-06-30 21:59:08 +0200

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,15 +4,13 @@
 Usage: ./2-post_email.py <URL> <email>
     - Displays the body of the response.
 """
-
 import sys
 import requests
 
+
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
+    value = {"email": sys.argv[2]}
 
-    data = {"email": email}
-    response = requests.post(url, data=data)
-
-    print(response.text)
+    r = requests.post(url, data=value)
+    print(r.text)
