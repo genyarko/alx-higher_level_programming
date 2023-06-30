fa1fc8d03f507ebf848393c588559792ebfd9a64
genyarko <115112347+genyarko@users.noreply.github.com>
2023-06-30 21:56:59 +0200

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-"""Sends a POST request to a given URL with an email as a parameter and displays the response body.
-
-Usage: ./2-post_email.py <URL> <email>
-    - Sends a POST request to the specified URL with the email as a parameter.
-    - Displays the body of the response.
+"""Sends a POST request to a given URL with a given email.
+Usage: ./6-post_email.py <URL> <email>
+  - Displays the body of the response.
 """
-
 import sys
 import requests
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
+    value = {"email": sys.argv[2]}
 
-    payload = {"email": email}
-    response = requests.post(url, data=payload)
-
-    print(response.text)
+    r = requests.post(url, data=value)
+    print(r.text)
