9edca4189d89d041e265caf4a6450f2d6522a5f5
genyarko <115112347+genyarko@users.noreply.github.com>
2023-07-25 17:59:36 +0200

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,17 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-// Get the file path and content from command line arguments
-const filePath = process.argv[2];
-const fileContent = process.argv[3];
-
-// Write the file content to the specified file path
-fs.writeFile(filePath, fileContent, (err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(`File ${filePath} written successfully.`);
-  }
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
-
